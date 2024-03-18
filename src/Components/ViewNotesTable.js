b40d042f5b1cb442a48efc3ee817bd739bc62423
Ditu <oyasiul.islam_ditu@yahoo.com>
2024-03-18 08:05:52 +0600

    sorting added

--- src/Components/ViewNotesTable.js
+++ src/Components/ViewNotesTable.js
@@ -1,19 +1,70 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { Button, ButtonGroup } from 'react-bootstrap';
 import Table from 'react-bootstrap/Table';
 import { FormModal } from './FormModal';
 
-export const ViewNotesTable = ({notes,viewNotes, deleteNote, addNoteToTable, setNotes, updateNoteToTable}) => {
 
-console.log(viewNotes)
+export const ViewNotesTable = ({notes,viewNotes,setViewNotes, deleteNote, addNoteToTable, setNotes, updateNoteToTable}) => {
+const sorting = useRef();
+const statusVal = {
+  "Pending" : 1,
+  "In Progress" : 2,
+  "Completed" : 3,
+  "Failed" : 4
+}
+
+
+const sortAscending =(isAsc) =>{
+  const sortProperty =sorting.current.value;
+  let temp = [...viewNotes];
+  
+  if(sortProperty === 'status'){
+    temp.sort((a,b) => {
+      return isAsc? statusVal[a[sortProperty]] - statusVal[b[sortProperty]] :
+               statusVal[b[sortProperty]] - statusVal[a[sortProperty]]
+    })
+  } else{
+      temp.sort((a,b) => {
+      return isAsc ? a[sortProperty].localeCompare(b[sortProperty]):
+               b[sortProperty].localeCompare(a[sortProperty])
+    })
+  }
+  
+  console.log(temp)
+  setViewNotes(temp)
+
+}
+
+const undoSorting = () => {
+  setViewNotes(notes)
+}
+
     return (
         <div className='container mt-3'>
+        <div className='d-flex justify-content-between'>
         <FormModal addNoteToTable={addNoteToTable}  heading="Add New Note" buttonName="Add note"/>
+        <div>
+          <select className='py-1 me-2' ref={sorting} defaultValue="title">
+            <option value="title">Title</option>
+            <option value="description">Description</option>
+            <option value="priority">priority</option>
+            <option value="createdAt">Created At</option>
+            <option value="updatedAt">Updated At</option>
+            <option value="status">Status</option>
+          </select>
+
+          <button onClick={() => sortAscending(true)} className='me-2'>ASC</button>
+          <button onClick={() => sortAscending(false)} className='me-2'>DESC</button>
+          <button onClick={() => undoSorting()} className='me-2'>Undo Sorting</button>
+        </div>
+        </div>
         <Table className='mt-2' striped responsive bordered hover>
           <thead>
             <tr>
               <th>ID</th>
-              <th>Title</th>
+              <th>Title
+              
+              </th>
               <th>Description</th>
               <th>priority</th>
               <th>Created At</th>
--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import reportWebVitals from './reportWebVitals';
 import router from './Routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-  
+
+
 import {
   RouterProvider,
 } from "react-router-dom";
